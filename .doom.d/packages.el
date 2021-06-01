3cfe83c83445a2d723fce35f234697ec77d80af1
Phillip O'Reggio <pnoreggio@gmail.com>
2021-05-31 22:27:56 -0400

    Change google funcs, make epaint easier to use, style in elisp segments

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -12,9 +12,7 @@
 ;; ===== functionality
 (package! company-tabnine)              ;; Completion + Deep Learning
 (package! epaint :recipe                ;; paint drawing for emacs
-  (:host github
-   :repo "chuntaro/epaint"
-   :files ("*")))
+  (:host github :repo "chuntaro/epaint" :files ("*")))
 (package! evil-quickscope)              ;; Target highlighting for f/t jumps
 (package! forge)                        ;; Work with git forges (like reviewing PRs)
 (package! webkit-color-picker)          ;; Selct colors from a palette (using webkit)
@@ -23,39 +21,37 @@
 (package! vmd-mode)                     ;; Preview markdown
 
 ;; ===== Latex
-(package! webkit-katex-render :recipe   ;; Show preview of equation in popup
-  (:host github
-   :repo "fuxialexander/emacs-webkit-katex-render"))
+(package! webkit-katex-render    ;; Show preview of equation in popup
+  :recipe (:host github :repo "fuxialexander/emacs-webkit-katex-render"))
 
 ;; ===== language support
 (package! flycheck-swiftlint)           ;; Swiftlint
 (package! graphql-mode)                 ;; Syntax support for the graphql query language
-(package! harmony-mode :recipe          ;; Syntax support for Harmony, a language used in CS 4410 at Cornell
-  (:local-repo "local_packages/harmony-mode"))
+(package! harmony-mode                  ;; Syntax support for Harmony, a language used in CS 4410 at Cornell
+  :recipe (:local-repo "local_packages/harmony-mode"))
+
 (package! lsp-sourcekit)                ;; LSP suppport for sourcekit (swift)
 
 ;; ===== OR tools
 (package! ampl-mode :recipe             ;; Syntax support for AMPL
-  (:host github
-   :repo "dpo/ampl-mode"
-   :files ("emacs/ampl-mode.el")))
+  (:host github :repo "dpo/ampl-mode" :files ("emacs/ampl-mode.el")))
 
 ;; ===== Json
 (package! json-navigator)               ;; Navigate json with tree structure
 
 ; ===== Org
 (package! ord-d20 :recipe               ;; keybinds for table top games
-  (:host github
-   :repo "spwhitton/org-d20"))
+  (:host github :repo "spwhitton/org-d20"))
 
 ;; ===== Misc / Fun
 (package! font-lock-studio)             ;; Debugging font-lock regexes
 (package! zone-rainbow)                 ;; Another zone program
-(package! play-sound-osx :recipe        ;; Sound support in emacs
-  (:host github
-   :repo "leoliu/play-sound-osx"))
-(package! tetris-sounds :recipe         ;; Sounds for emacs tetris
-  (:local-repo "local_packages/tetris-sounds"))
+(package! play-sound-osx                ;; Sound support in emacs
+  :recipe (:host github :repo "leoliu/play-sound-osx"))
+(package! tetris-sounds                 ;; Sounds for emacs tetris
+  :recipe (:local-repo "local_packages/tetris-sounds"))
 (package! leetcode)                     ;; Leetcode
-(package! huecycle :recipe              ;; Colorize fonts
- (:local-repo "local_packages/huecycle"))
+(package! huecycle              ;; Colorize fonts
+  :recipe (:local-repo "local_packages/huecycle"))
+(package! svg-tag-mode
+  :recipe (:host github :repo "rougier/svg-tag-mode"))
