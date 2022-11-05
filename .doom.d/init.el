baa62e411ce0f47a5da066cc6e4e8db2586c0909
Phillip O'Reggio <pnoreggio@gmail.com>
2022-11-04 22:31:28 -0400

    bump on stuff

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -45,7 +45,7 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        tabs              ; a tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       (treemacs +lsp)          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -193,3 +193,9 @@
        :config
        literate
        (default +bindings +smartparens))
+
+;; Native compile
+(setq native-comp-deferred-compilation nil)
+(setq package-native-compile t)
+(after! (doom-packages straight)
+  (setq straight--native-comp-available t))
