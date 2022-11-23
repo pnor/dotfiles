319e03d847a72e0f384fa69896441b130684b8a7
Phillip O'Reggio <pnoreggio@gmail.com>
2022-11-22 22:36:21 -0500

    periodic bumps

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -9,16 +9,19 @@
 (package! rainbow-mode)                 ;; Preview color codes in-line
 
 ;; ===== functionality
+(package! blamer)                       ;; Show git blame on line
+(package! demap)                        ;; Code Minimap
 (package! epaint                        ;; paint drawing for emacs
   :recipe (:host github :repo "chuntaro/epaint" :files ("*")))
 (package! evil-quickscope)              ;; Target highlighting for f/t jumps
-(package! forge)                        ;; Work with git forges (like reviewing PRs)
 (package! floatbuf                      ;; Floating buffers
   :recipe (:host github :repo "pnor/floatbuf"))
-(package! realgud-lldb)                 ;; lldb support for debugger
-(package! blamer)                       ;; Show git blame on line
-(package! demap)                        ;; Code Minimap
+(package! forge)                        ;; Work with git forges (like reviewing PRs)
+(package! gcmh)                         ;; Make emacs more responive by monkeying with the garbage collector
 (package! org-modern)                   ;; Org mode modern look
+(package! realgud-lldb)                 ;; lldb support for debugger
+(package! vlf :recipe (:host github :repo "m00natic/vlfi" :files ("*.el"))
+  :pin "cc02f2533782d6b9b628cec7e2dcf25b2d05a27c") ;; Open huge viles incrementally
 
 ;; ===== language support
 (package! flycheck-swiftlint)           ;; Swiftlint
@@ -27,8 +30,6 @@
 (package! docker)                       ;; Docker
 (package! dockerfile-mode)              ;; Docker Syntax
 (package! clang-format)                 ;; C family formatting
-(package! tree-sitter)                  ;; Tree Sitter
-(package! tree-sitter-langs)
 (package! nhexl-mode)                   ;; Better hexl mode
 
 ;; ===== OR tools
@@ -51,10 +52,15 @@
 (package! huecycle)                             ;; Colorize fonts
 (package! play-sound-osx :ignore IS-LINUX       ;; Sound support in emacs
   :recipe (:host github :repo "leoliu/play-sound-osx"))
-(package! vlc)                          ;; Music controlled by Emacs
-(package! random-theme                 ;; Randomize my color theme
+(package! vlc)                                  ;; Music controlled by Emacs
+(package! random-theme                          ;; Randomize my color theme
  :recipe (:host github :repo "pnor/random-theme"))
+(package! power-mode                            ;; POWER
+  :recipe (:host github :repo "elizagamedev/power-mode.el"))
+(package! leetcode)                             ;; Leetcode but in place where I can bear to do it
 
 ;; ===== Conditional
 (package! ewal :ignore IS-MAC)
 (package! ewal-doom-themes :ignore IS-MAC)
+
+
