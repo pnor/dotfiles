368591a530bc0e5e26ebd23ec6fd8d9435af4b77
pnor <pnoreggio@gmail.com>
2025-08-18 10:10:53 -0700

    more config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import yaml
+
+config.load_autoconfig()
+
+with (config.configdir / "qutebrowser.yml").open() as f:
+    yaml_data = yaml.safe_load(f)
+
+
+def dict_attrs(obj, path=""):
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            yield from dict_attrs(v, "{}.{}".format(path, k) if path else k)
+    else:
+        yield path, obj
+
+
+for k, v in dict_attrs(yaml_data):
+    config.set(k, v)
+
+c.content.blocking.enabled = True
+c.content.blocking.method = "adblock"
+c.content.blocking.adblock.lists = [
+    "https://github.com/ewpratten/youtube_ad_blocklist/blob/master/blocklist.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt",
+]
+
+# privacy - adjust these settings based on your preference
+
+# config.set("completion.cmd_history_max_items", 0)
+# want search history ..
+# config.set("content.private_browsing", True)
+# config.set("content.webgl", False, "*")
+config.set("content.canvas_reading", False)
+config.set("content.geolocation", False)
+config.set("content.webrtc_ip_handling_policy", "default-public-interface-only")
+config.set("content.cookies.accept", "all")
+config.set("content.cookies.store", True)
